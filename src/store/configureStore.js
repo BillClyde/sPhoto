7ef8e38b6b317fd1d40dccae8e3dedc6936029ae
William Clyde <bill.clyde@gmail.com>
2018-06-27 16:49:35 -0700

    Added import and export scaffolding

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,26 +1,31 @@
-import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
-import { createSagaMiddleware } from 'redux-saga';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux'
+import { createSagaMiddleware } from 'redux-saga'
+
+import * as Import from './Import'
+import * as Export from './Export'
 
 export default function configureStore(history, initialState) {
   const reducers = {
-  };
+    import: Import.reducer,
+    export: Export.reducer
+  }
   const sagaMiddleware = createSagaMiddleware();
   const middleware = [
     sagaMiddleware
-  ];
+  ]
 
   const enhancers = []
 
   const rootReducer = combineReducers({
     ...reducers
-  });
+  })
   const store = createStore(
     rootReducer,
     initialState,
     compose(applyMiddleware(...middleware), ...enhancers)
-  );
+  )
 
-  sagaMiddleware.run(rootSaga);
+  sagaMiddleware.run(rootSaga)
   
-  return store;
+  return store
 }
