56b7683377def85d95c954fe81346b6f21956061
William Clyde <bill.clyde@gmail.com>
2018-07-10 16:40:39 -0700

    Added a temporary backend to push development forward

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,11 +1,25 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects'
 
+import {IMPORT_FILES, PREVIEW_FILES, SELECT_FILE} from '../store/Import'
+
 export function helloSaga(){
   console.log('Hello Sagas!')
 }
 
 export function* watchNavigation(){
   yield takeLatest('NAVIGATION', navigation)
+}
+
+export function* watchImportFiles(){
+  yield takeLatest(IMPORT_FILES, () => console.log('Import Files called'))
+}
+
+export function* watchPreviewFiles(){
+  yield takeLatest(PREVIEW_FILES, () => console.log('Preview Files called'))
+}
+
+export function* watchSelectFile(){
+  yield takeLatest(SELECT_FILE, (file) => console.log('Select File called'))
 }
 
 export function navigation(){
@@ -18,6 +32,9 @@ export function navigation(){
 export default function* root(){
   yield all([
     helloSaga(),
-    watchNavigation()
+    watchNavigation(),
+    watchImportFiles(),
+    watchPreviewFiles(),
+    watchSelectFile()
   ]);
 }
